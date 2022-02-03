01aa2ec79ceda643ee5eb7701c2e1a82cbc6627f
ruuushhh <ruuushhh2001@gmail.com>
2022-02-03 11:12:49 +0530

    Home page started

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_catalog/home_page.dart';
+import 'home_page.dart';
 
 void main() {
   runApp(MyApp());
-  }
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:  Material(
-        child: Center(
-          child: Container(
-            child: Text('Welocome to my App'),
-      ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
